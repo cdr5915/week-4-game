a7de66dff1db96e6acac4254e62fe6ad146d8e5d
Caroline Hurley <carolinehurley@carolines-imac.attlocal.net>
2017-02-24 16:45:47 -0500

    Working Game, two bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,18 @@
 
-// var randomCupcakeNum1 = 0;
-// var randomCupcakeNum2 = 0;
-// var randomCupcakeNum3 = 0;
-// var randomCupcakeNum4 = 0;
-
-
-// $("#cupcake-1").on("click", function(){
-// 	if (randomCupcakeNum1 === 0) {
-//     randomCupcakeNum1 = Math.floor(Math.random() * ((12-1) +1) + 1);
-//     console.log("button number 1: " + randomCupcakeNum1);}
-//     $("#sum-numbers-box").html(randomCupcakeNum1);
-// });
-
-
-// $("#cupcake-2").on("click", function(){
-// 	if (randomCupcakeNum2 === 0) {
-//     randomCupcakeNum2 = Math.floor(Math.random() * ((12-1) +1) + 1);
-//     console.log("button number 2: " + randomCupcakeNum2);}
-//     $("#sum-numbers-box").html(randomCupcakeNum2);
-// });
-
-
-// $("#cupcake-3").on("click", function(){
-// 	if (randomCupcakeNum3 === 0) {
-//     randomCupcakeNum3 = Math.floor(Math.random() * ((12-1) +1) + 1);
-//     console.log("button number 3: " + randomCupcakeNum3);}
-//     $("#sum-numbers-box").html(randomCupcakeNum3);
-// });
-
-
-// $("#cupcake-4").on("click", function(){
-// 	if (randomCupcakeNum4 === 0) {
-//     randomCupcakeNum4 = Math.floor(Math.random() * ((12-1) +1) + 1);
-//     console.log("button number 4: " + randomCupcakeNum4);}
-//     $("#sum-numbers-box").html(randomCupcakeNum4);
-// });
-
-// });
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
 var cupcakeNums = [];
 var counter = 0;
 
+var resetGame = function () {
+    counter = 0;
+    $("#sum").html(counter);
+    computerNum = Math.floor(Math.random() * ((120-19) + 1) + 19);
+    $("#comp-number").html(computerNum);
+    console.log(computerNum)
+    
+}
 
 $(document).ready(function() {
 
@@ -54,20 +24,44 @@ $(document).ready(function() {
     //making array of random numbers for cupcakes
     for (var i = 0; i<4; i++) {
     cupcakeNums[i] = Math.floor(Math.random() * ((12-1) + 1) + 1);
-    }
-
-    console.log(cupcakeNums);
+    console.log(i + " random number: " + cupcakeNums[i]);
 
     //assigning random num to each cupcake by ID
     $("#cupcake-" + i ).attr("data-cupcakevalue", cupcakeNums[i]);
+    }
 
-    //Extracting the cupcake value of the clicked cupcake.
+
+    //Extracting the cupcake value of the clicked cupcake and converting to integer.
     $(".cupcake-button").on("click", function(){
         var cupcakeValue = ($(this).attr("data-cupcakevalue"));
         cupcakeValue = parseInt(cupcakeValue);
+        console.log("cupcake value: " + cupcakeValue);
 
-    //Add new value of click to counter variable
-    counter += cupcakeValue;
+        //Add new value of click to counter variable
+        counter += cupcakeValue;
+
+        console.log("New score: " + counter);
+        //Displaying sum of clicks 
+        $("#sum").html(counter);
+
+        if(counter === computerNum) {
+            alert("You win!");
+            wins++
+            console.log("wins: " + wins);
+            $("#wins").html("Wins: "+ wins);
+            resetGame();
+        }
+
+        else if (counter >= computerNum) {
+            alert("You lose!");
+            losses++
+            console.log("losses: " + losses);
+            $("#losses").html("Losses: "+ losses);
+            resetGame();
+
+        }
+
+
     });
 
 });
